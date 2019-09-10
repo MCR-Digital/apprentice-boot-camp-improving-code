e2a28ebd8306ef05f853e3c572453a481498b1f5
Brandon Craig <BrandonCraig@rentalcars.com>
2019-09-10 15:42:31 +0100

    Removed Position from player class

--- exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Player.java
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Player.java
@@ -3,13 +3,11 @@ package com.adaptionsoft.games.uglytrivia;
 public class Player {
 
     private int coinCount;
-    private int position;
     private boolean isInPenaltyBox;
     private String name;
 
     public Player(String name) {
         coinCount = 0;
-        position = 0;
         isInPenaltyBox = false;
         this.name = name;
     }
@@ -18,9 +16,6 @@ public class Player {
         return coinCount;
     }
 
-    public int getPosition() {
-        return position;
-    }
 
     public boolean isInPenaltyBox() {
         return isInPenaltyBox;
