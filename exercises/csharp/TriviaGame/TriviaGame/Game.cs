f39571a4aeac7b742e30284f65df9124382700f7
Christopher Gidney <chris.gidney@mccann.com>
2019-09-10 14:55:46 +0100

    Removed method

--- exercises/csharp/TriviaGame/TriviaGame/Game.cs
+++ exercises/csharp/TriviaGame/TriviaGame/Game.cs
@@ -27,13 +27,8 @@ namespace TriviaGame
                 popQuestions.AddLast("Pop Question " + i);
                 scienceQuestions.AddLast(("Science Question " + i));
                 sportsQuestions.AddLast(("Sports Question " + i));
-                rockQuestions.AddLast(CreateRockQuestion(i));
+                rockQuestions.AddLast("Rock Question " + i);
             }
-        }
-
-        public string CreateRockQuestion(int index)
-        {
-            return "Rock Question " + index;
         }
 
         public bool IsPlayable()
