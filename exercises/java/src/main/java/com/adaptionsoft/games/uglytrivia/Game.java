32c407e73861f64ef8affbbca2dc5085f775a96a
Georgia Carr <georgia.carr@autotrader.co.uk>
2019-09-10 15:46:28 +0100

    pulling out loop of players

--- exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -66,7 +66,6 @@ public class Game {
         } else {
             outcomeOfCorrectAnswer(numberOnDiceAfterRoll);
         }
-
     }
 
     private void outcomeOfCorrectAnswer(int numberOnDiceAfterRoll) {
