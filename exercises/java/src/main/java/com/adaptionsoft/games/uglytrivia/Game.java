df2dd7170ca6703a9494e8b8088bf807f7588d2f
Brandon Craig <BrandonCraig@rentalcars.com>
2019-09-10 14:32:26 +0100

    Made askQuestion() take advantage of my hashmap

--- exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -100,14 +100,7 @@ public class Game {
 	}
 
 	private void askQuestion() {
-		if (currentCategory() == "Pop")
-			System.out.println(popQuestions.removeFirst());
-		if (currentCategory() == "Science")
-			System.out.println(scienceQuestions.removeFirst());
-		if (currentCategory() == "Sports")
-			System.out.println(sportsQuestions.removeFirst());
-		if (currentCategory() == "Rock")
-			System.out.println(rockQuestions.removeFirst());		
+		System.out.println(topicQuestionMap.get(currentCategory()).removeFirst());
 	}
 	
 	
