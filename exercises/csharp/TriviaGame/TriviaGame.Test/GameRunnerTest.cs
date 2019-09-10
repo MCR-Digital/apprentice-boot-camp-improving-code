928b70cb36d96ca6fd2f4a8efc6102c9dd701671
Tristan <tristan@Tristans-MacBook-Pro.local>
2019-09-10 11:46:57 +0100

    Split code into two projects to run with .Net cli
    Ported the code to .Net Core

---
+++ exercises/csharp/TriviaGame/TriviaGame.Test/GameRunnerTest.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using System;
+using System.IO;
+using TriviaGame.Core;
+
+namespace TriviaGame
+{
+  public class GameRunnerTest
+  {
+    private StringWriter _writer;
+
+    [SetUp]
+    public void Setup()
+    {
+      _writer = new StringWriter();
+      Console.SetOut(_writer);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      _writer.Close();
+      Console.Out.Close();
+      Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+    }
+
+    [Test]
+    public void GoldenMasterTest()
+    {
+      for (int i = 0; i < 10000; i++)
+      {
+        GameRunner.Main(new string[] { i.ToString() });
+
+        var expectedContent = _writer.ToString();
+        var actualContent = GoldenMaster.GetResult(i);
+
+        Assert.That(actualContent, Is.EqualTo(expectedContent));
+
+        _writer.GetStringBuilder().Clear();
+      }
+    }
+  }
+}
