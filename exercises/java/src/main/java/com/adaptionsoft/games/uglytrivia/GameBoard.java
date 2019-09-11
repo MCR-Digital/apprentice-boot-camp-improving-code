71045c90b066c2dec5cc9f3231f966441a4fb4f2
Joe.Hayward <joseph.hayward@autotrader.co.uk>
2019-09-11 14:42:18 +0100

    Created GameBoardPosition class & populated GameBoard with GameBoardPositions

--- exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/GameBoard.java
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/GameBoard.java
@@ -1,4 +1,24 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameBoard {
+
+    List<GameBoardPosition> board = new ArrayList<>();
+
+    public GameBoard() {
+        board.add(new GameBoardPosition("Pop"));
+        board.add(new GameBoardPosition("Science"));
+        board.add(new GameBoardPosition("Sports"));
+        board.add(new GameBoardPosition("Rock"));
+        board.add(new GameBoardPosition("Pop"));
+        board.add(new GameBoardPosition("Science"));
+        board.add(new GameBoardPosition("Sports"));
+        board.add(new GameBoardPosition("Rock"));
+        board.add(new GameBoardPosition("Pop"));
+        board.add(new GameBoardPosition("Science"));
+        board.add(new GameBoardPosition("Sports"));
+        board.add(new GameBoardPosition("Rock"));
+    }
 }
--- exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/GameBoardPosition.java
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/GameBoardPosition.java
@@ -2,4 +2,8 @@ package com.adaptionsoft.games.uglytrivia;
 
 public class GameBoardPosition {
     String category;
+
+    public GameBoardPosition(String category) {
+        this.category = category;
+    }
 }
