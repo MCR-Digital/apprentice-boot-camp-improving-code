2a604aa850b65a5b5fb29299461120df2c173b98
Brandon Craig <BrandonCraig@rentalcars.com>
2019-09-11 14:34:54 +0100

    Created fields and constructor for Board class

--- exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Board.java
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Board.java
@@ -1,6 +1,17 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import java.util.ArrayList;
+
 public class Board {
-    String[] board= {"Pop", "Science", "Sports", "Rock", "Pop", "Science", "Sports", "Rock", "Pop", "Science", "Sports", "Rock"};
+
+    String[] boardSquares= {"Pop", "Science", "Sports", "Rock"};
+    ArrayList<Player> players = new ArrayList<>();
+    int[] playerBoardPosition = new int[6];
+    QuestionDeck questionDeck;
+
+    public Board() {
+        questionDeck = new QuestionDeck();
+    }
+
 
 }
