bfea13b12228fd8f2cd34591d028ab2c0645e20f
maxswiftdev <biimtec@gmail.com>
2019-09-11 14:31:42 +0100

    Created Player and Board classes

--- exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Board.java
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Board.java
@@ -1,7 +1,9 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import java.util.ArrayList;
+
 public class Board {
-
-
-
+    private String[] categories = {"Pop", "Science", "Sports", "Rock"};
+    private int numOfPlaces = 12;
+    private ArrayList<Player> penaltyBox = new ArrayList<>();
 }
--- exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Player.java
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Player.java
@@ -1,4 +1,31 @@
 package com.adaptionsoft.games.uglytrivia;
 
 public class Player {
+    private String name;
+    private int coin = 0;
+    private int placeOnBoard = 0;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCoin() {
+        return coin;
+    }
+
+    public int getPlaceOnBoard() {
+        return placeOnBoard;
+    }
+
+    public void addCoin() {
+        this.coin++;
+    }
+
+    public void setPlaceOnBoard(int placeDisplacement) {
+        placeOnBoard += placeDisplacement;
+    }
 }
