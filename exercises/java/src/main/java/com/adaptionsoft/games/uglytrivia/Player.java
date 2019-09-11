286efa2dc89833ea216a8a72b045e3cc9c0bf29f
Joe.Hayward <joseph.hayward@autotrader.co.uk>
2019-09-11 15:38:23 +0100

    Player class - removed unused method & added playerPositionOnBoard

--- exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Player.java
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Player.java
@@ -2,13 +2,11 @@ package com.adaptionsoft.games.uglytrivia;
 
 public class Player {
     public String name;
+    public int playerPositionOnBoard;
 
     public Player(String name) {
         this.name = name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        this.playerPositionOnBoard = 0;
     }
 
     public String getName() {
