e6a93c8159f1dc856618a58dae45b6a47f395b49
jameson-bell-itv <jameson.bell1@itv.com>
2019-09-11 14:01:42 +0100

    Created new classes to begin extracting responsibility from Game class

---
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Player.java
@@ -0,0 +1,15 @@
+package com.adaptionsoft.games.uglytrivia;
+
+public class Player {
+
+	private String playerName;
+	private int locationOnBoard;
+	private int totalCoins;
+	private boolean isInPenaltyBox;
+
+	public Player(String playerName){
+		this.playerName = playerName;
+	}
+
+
+}
---
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Question.java
@@ -0,0 +1,11 @@
+package com.adaptionsoft.games.uglytrivia;
+
+public class Question {
+
+	private static final String CATEGORY_POP = "Pop";
+	private static final String CATEGORY_SCIENCE = "Science";
+	private static final String CATEGORY_SPORTS = "Sports";
+	private static final String CATEGORY_ROCK = "Rock";
+
+
+}
