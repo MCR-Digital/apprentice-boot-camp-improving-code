4295e389f5f3c8b552eb3a732de3e768da9a8dfb
Brandon Craig <BrandonCraig@rentalcars.com>
2019-09-11 14:28:42 +0100

    Created a Board class

---
+++ exercises/java/src/main/java/com/adaptionsoft/games/uglytrivia/Board.java
@@ -0,0 +1,6 @@
+package com.adaptionsoft.games.uglytrivia;
+
+public class Board {
+    String[] board= {"Pop", "Science", "Sports", "Rock", "Pop", "Science", "Sports", "Rock", "Pop", "Science", "Sports", "Rock"};
+
+}
