46ff81fb7d79aa0966887771f08cc78804764329
Tom Durrant <tomchdurrant@gmail.com>
2020-09-21 14:11:40 +0100

    Remove Add Rock Question

--- exercises/csharp/TriviaGame/TriviaGame/Game.cs
+++ exercises/csharp/TriviaGame/TriviaGame/Game.cs
@@ -30,15 +30,10 @@ namespace TriviaGame
             for (int i = 0; i < 50; i++)
             {
                 popQuestions.AddLast("Pop Question " + i);
-                scienceQuestions.AddLast(("Science Question " + i));
-                sportsQuestions.AddLast(("Sports Question " + i));
-                rockQuestions.AddLast(CreateRockQuestion(i));
+                scienceQuestions.AddLast("Science Question " + i);
+                sportsQuestions.AddLast("Sports Question " + i);
+                rockQuestions.AddLast("Rock Question " + i);
             }
-        }
-
-        public string CreateRockQuestion(int index)
-        {
-            return "Rock Question " + index;
         }
 
         public bool IsPlayable()
@@ -48,8 +43,6 @@ namespace TriviaGame
 
         public bool Add(string playerName)
         {
-
-
             players.Add(playerName);
             places[players.Count] = 0;
             purses[players.Count] = 0;
@@ -76,8 +69,8 @@ namespace TriviaGame
                     if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
 
                     Console.WriteLine(players[currentPlayer]
-                            + "'s new location is "
-                            + places[currentPlayer]);
+                                      + "'s new location is "
+                                      + places[currentPlayer]);
                     Console.WriteLine("The category is " + CurrentCategory());
                     AskQuestion();
                 }
@@ -86,21 +79,18 @@ namespace TriviaGame
                     Console.WriteLine(players[currentPlayer] + " is not getting out of the penalty box");
                     isGettingOutOfPenaltyBox = false;
                 }
-
             }
             else
             {
-
                 places[currentPlayer] = places[currentPlayer] + roll;
                 if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
 
                 Console.WriteLine(players[currentPlayer]
-                        + "'s new location is "
-                        + places[currentPlayer]);
+                                  + "'s new location is "
+                                  + places[currentPlayer]);
                 Console.WriteLine("The category is " + CurrentCategory());
                 AskQuestion();
             }
-
         }
 
         private static bool CanLeavePenaltyBox(int roll)
@@ -115,16 +105,19 @@ namespace TriviaGame
                 Console.WriteLine(popQuestions.First());
                 popQuestions.RemoveFirst();
             }
+
             if (CurrentCategory() == "Science")
             {
                 Console.WriteLine(scienceQuestions.First());
                 scienceQuestions.RemoveFirst();
             }
+
             if (CurrentCategory() == "Sports")
             {
                 Console.WriteLine(sportsQuestions.First());
                 sportsQuestions.RemoveFirst();
             }
+
             if (CurrentCategory() == "Rock")
             {
                 Console.WriteLine(rockQuestions.First());
@@ -156,9 +149,9 @@ namespace TriviaGame
                     Console.WriteLine("Answer was correct!!!!");
                     purses[currentPlayer]++;
                     Console.WriteLine(players[currentPlayer]
-                            + " now has "
-                            + purses[currentPlayer]
-                            + " Gold Coins.");
+                                      + " now has "
+                                      + purses[currentPlayer]
+                                      + " Gold Coins.");
 
                     bool winner = DidPlayerWin();
                     currentPlayer++;
@@ -172,19 +165,15 @@ namespace TriviaGame
                     if (currentPlayer == players.Count) currentPlayer = 0;
                     return true;
                 }
-
-
-
             }
             else
             {
-
                 Console.WriteLine("Answer was corrent!!!!");
                 purses[currentPlayer]++;
                 Console.WriteLine(players[currentPlayer]
-                        + " now has "
-                        + purses[currentPlayer]
-                        + " Gold Coins.");
+                                  + " now has "
+                                  + purses[currentPlayer]
+                                  + " Gold Coins.");
 
                 bool winner = DidPlayerWin();
                 currentPlayer++;
@@ -208,8 +197,7 @@ namespace TriviaGame
 
         private bool DidPlayerWin()
         {
-            return !(purses[currentPlayer] == WinningScore);
+            return purses[currentPlayer] != WinningScore;
         }
     }
-
 }
