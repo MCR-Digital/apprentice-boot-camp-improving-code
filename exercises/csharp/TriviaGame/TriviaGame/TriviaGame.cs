616d09c62afba32d35cbe45f6ebec23251fd7d8a
Duncan Joyce <Duncan_Joyce@waters.com>
2020-09-21 14:51:54 +0100

    Initial removal of duplication

--- exercises/csharp/TriviaGame/TriviaGame/TriviaGame.cs
+++ exercises/csharp/TriviaGame/TriviaGame/TriviaGame.cs
@@ -6,6 +6,11 @@ namespace TriviaGame
 {
     public class TriviaGame
     {
+        private const int WINNING_SCORE = 6;
+        private const string POP_CATEGORY = "Pop";
+        private const string SCIENCE_CATEGORY = "Science";
+        private const string SPORTS_CATEGORY = "Sports";
+        private const string ROCK_CATEGORY = "Rock";
         List<string> players = new List<string>();
 
         int[] playerLocations = new int[6];
@@ -44,8 +49,6 @@ namespace TriviaGame
 
         public bool AddPlayer(string playerName)
         {
-
-
             players.Add(playerName);
             playerLocations[NumberOfPlayers()] = 0;
             playerPurses[NumberOfPlayers()] = 0;
@@ -73,74 +76,62 @@ namespace TriviaGame
                     isGettingOutOfPenaltyBox = true;
 
                     Console.WriteLine(players[currentPlayer] + " is getting out of the penalty box");
-                    playerLocations[currentPlayer] = playerLocations[currentPlayer] + roll;
-                    if (playerLocations[currentPlayer] > 11) playerLocations[currentPlayer] = playerLocations[currentPlayer] - 12;
-
-                    Console.WriteLine(players[currentPlayer]
-                            + "'s new location is "
-                            + playerLocations[currentPlayer]);
-                    Console.WriteLine("The category is " + CurrentCategory());
-                    AskQuestion();
+                    PlayTurn(roll);
                 }
                 else
                 {
                     Console.WriteLine(players[currentPlayer] + " is not getting out of the penalty box");
                     isGettingOutOfPenaltyBox = false;
                 }
-
             }
             else
             {
-
-                playerLocations[currentPlayer] = playerLocations[currentPlayer] + roll;
-                if (playerLocations[currentPlayer] > 11) playerLocations[currentPlayer] = playerLocations[currentPlayer] - 12;
-
-                Console.WriteLine(players[currentPlayer]
-                        + "'s new location is "
-                        + playerLocations[currentPlayer]);
-                Console.WriteLine("The category is " + CurrentCategory());
-                AskQuestion();
+                PlayTurn(roll);
             }
+        }
 
+        private void PlayTurn(int roll)
+        {
+            playerLocations[currentPlayer] = playerLocations[currentPlayer] + roll;
+            if (playerLocations[currentPlayer] > 11) playerLocations[currentPlayer] = playerLocations[currentPlayer] - 12;
+
+            Console.WriteLine(players[currentPlayer]
+                    + "'s new location is "
+                    + playerLocations[currentPlayer]);
+            Console.WriteLine("The category is " + CurrentCategory());
+            AskQuestion();
         }
 
         private void AskQuestion()
         {
-            if (CurrentCategory() == "Pop")
+            if (CurrentCategory() == POP_CATEGORY)
             {
                 Console.WriteLine(popQuestions.First());
                 popQuestions.RemoveFirst();
             }
-            if (CurrentCategory() == "Science")
+            if (CurrentCategory() == SCIENCE_CATEGORY)
             {
                 Console.WriteLine(scienceQuestions.First());
                 scienceQuestions.RemoveFirst();
             }
-            if (CurrentCategory() == "Sports")
+            if (CurrentCategory() == SPORTS_CATEGORY)
             {
                 Console.WriteLine(sportsQuestions.First());
                 sportsQuestions.RemoveFirst();
             }
-            if (CurrentCategory() == "Rock")
+            if (CurrentCategory() == ROCK_CATEGORY)
             {
                 Console.WriteLine(rockQuestions.First());
                 rockQuestions.RemoveFirst();
             }
         }
 
-
         private string CurrentCategory()
         {
-            if (playerLocations[currentPlayer] == 0) return "Pop";
-            if (playerLocations[currentPlayer] == 4) return "Pop";
-            if (playerLocations[currentPlayer] == 8) return "Pop";
-            if (playerLocations[currentPlayer] == 1) return "Science";
-            if (playerLocations[currentPlayer] == 5) return "Science";
-            if (playerLocations[currentPlayer] == 9) return "Science";
-            if (playerLocations[currentPlayer] == 2) return "Sports";
-            if (playerLocations[currentPlayer] == 6) return "Sports";
-            if (playerLocations[currentPlayer] == 10) return "Sports";
-            return "Rock";
+            if (playerLocations[currentPlayer] % 4 == 0) return POP_CATEGORY;
+            if (playerLocations[currentPlayer] % 4 == 1) return SCIENCE_CATEGORY;
+            if (playerLocations[currentPlayer] % 4 == 2) return SPORTS_CATEGORY;
+            return ROCK_CATEGORY;
         }
 
         public bool WasQuestionAnsweredCorrectly()
@@ -149,45 +140,33 @@ namespace TriviaGame
             {
                 if (isGettingOutOfPenaltyBox)
                 {
-                    Console.WriteLine("Answer was correct!!!!");
-                    playerPurses[currentPlayer]++;
-                    Console.WriteLine(players[currentPlayer]
-                            + " now has "
-                            + playerPurses[currentPlayer]
-                            + " Gold Coins.");
-
-                    bool winner = DidPlayerWin();
-                    currentPlayer++;
-                    if (currentPlayer == players.Count) currentPlayer = 0;
-
-                    return winner;
+                    return CorrectAnswer("Answer was correct!!!!");
                 }
                 else
                 {
-                    currentPlayer++;
-                    if (currentPlayer == players.Count) currentPlayer = 0;
+                    ChangeCurrentPlayer();
                     return true;
                 }
-
-
-
             }
             else
             {
-
-                Console.WriteLine("Answer was corrent!!!!");
-                playerPurses[currentPlayer]++;
-                Console.WriteLine(players[currentPlayer]
-                        + " now has "
-                        + playerPurses[currentPlayer]
-                        + " Gold Coins.");
-
-                bool winner = DidPlayerWin();
-                currentPlayer++;
-                if (currentPlayer == players.Count) currentPlayer = 0;
-
-                return winner;
+                return CorrectAnswer("Answer was corrent!!!!");
             }
+        }
+
+        private bool CorrectAnswer(string answerMessage)
+        {
+            Console.WriteLine(answerMessage);
+            playerPurses[currentPlayer]++;
+            Console.WriteLine(players[currentPlayer]
+                    + " now has "
+                    + playerPurses[currentPlayer]
+                    + " Gold Coins.");
+
+            bool winner = DoesPlayerHaveWinningScore();
+            ChangeCurrentPlayer();
+
+            return winner;
         }
 
         public bool WrongAnswer()
@@ -195,16 +174,19 @@ namespace TriviaGame
             Console.WriteLine("Question was incorrectly answered");
             Console.WriteLine(players[currentPlayer] + " was sent to the penalty box");
             inPenaltyBox[currentPlayer] = true;
-
-            currentPlayer++;
-            if (currentPlayer == players.Count) currentPlayer = 0;
+            ChangeCurrentPlayer();
             return true;
         }
 
-
-        private bool DidPlayerWin()
+        private void ChangeCurrentPlayer()
         {
-            return !(playerPurses[currentPlayer] == 6);
+            currentPlayer++;
+            if (currentPlayer == players.Count) currentPlayer = 0;
+        }
+
+        private bool DoesPlayerHaveWinningScore()
+        {
+            return !(playerPurses[currentPlayer] == WINNING_SCORE);
         }
     }
 
