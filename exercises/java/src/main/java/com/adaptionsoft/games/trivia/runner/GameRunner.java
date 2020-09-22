4bf7249fdc43ba7350dfceae8a065069973685a3
april <april.peace@autotrader.co.uk>
2020-09-22 11:56:54 +0100

    refactor

--- exercises/java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ exercises/java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,4 +1,3 @@
-
 package com.adaptionsoft.games.trivia.runner;
 import java.util.Random;
 
@@ -11,26 +10,26 @@ public class GameRunner {
 
 	public static void main(String[] args) {
 		Game aGame = new Game();
-		
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-		
+
+		aGame.addPlayer("Chet");
+		aGame.addPlayer("Pat");
+		aGame.addPlayer("Sue");
+
 		Random rand = new Random(Integer.parseInt(args[0]));
-	
+
 		do {
-			
+
 			aGame.roll(rand.nextInt(5) + 1);
-			
+
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
+				notAWinner = aGame.isCorrectAnswer();
 			}
-			
-			
-			
+
+
+
 		} while (notAWinner);
-		
+
 	}
 }
