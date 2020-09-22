34a623a4de15fa6cce345217e8101a4ebeae46c0
robyoung91 <robertmatthewyoung@gmail.com>
2020-09-22 14:44:15 +0100

    Amend logic in ProcessRoll to remove repeated code

--- exercises/csharp/TriviaGame/TriviaGame/Game.cs
+++ exercises/csharp/TriviaGame/TriviaGame/Game.cs
@@ -26,29 +26,24 @@ namespace TriviaGame
 
         public void ProcessRoll(int diceResult)
         {
-            players.currentPlayer = players.GetCurrentPlayer();
+            players.GetCurrentPlayer();
             Console.WriteLine(players.currentPlayer.Name + " is the current player");
             Console.WriteLine("They have rolled a " + diceResult);
 
-            if (players.currentPlayer.InPenaltyBox)
+            if (players.currentPlayer.InPenaltyBox && !IsDiceResultOdd(diceResult))
             {
-                if (IsDiceResultOdd(diceResult))
-                {
-                    players.currentPlayer.GettingOutOfPenaltyBox = true;
-                    Console.WriteLine(players.currentPlayer.Name + " is getting out of the penalty box");
-
-                    MovePlayer(diceResult);
-                    questions.AskQuestion(players.currentPlayer.Place);
-                }
-                else
-                {
-                    Console.WriteLine(players.currentPlayer.Name + " is not getting out of the penalty box");
-                    players.currentPlayer.GettingOutOfPenaltyBox = false;
-                }
+                Console.WriteLine(players.currentPlayer.Name + " is not getting out of the penalty box");
+                players.currentPlayer.GettingOutOfPenaltyBox = false;
 
             }
             else
             {
+                if (players.currentPlayer.InPenaltyBox)
+                {
+                    players.currentPlayer.GettingOutOfPenaltyBox = true;
+                    Console.WriteLine(players.currentPlayer.Name + " is getting out of the penalty box");
+                }
+
                 MovePlayer(diceResult);
                 questions.AskQuestion(players.currentPlayer.Place);
             }
