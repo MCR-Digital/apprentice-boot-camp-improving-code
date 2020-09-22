bbcee5ddb55a0fb9c15348c423bc9be56f607f3a
robyoung91 <robertmatthewyoung@gmail.com>
2020-09-22 13:31:20 +0100

    Add new Questions class
    * Move question lists to Questions class
    
    * Remove question seeding functionality from Game method and move to
    	new method in Questions, SeedQuestions()
    
    * Remove CurrentCategory() from Game class and move to new method
    	in Questions, GetCurrentQuestionCategory()
    
    * Move AskQuestion method to Questions class
    
    * Remove CreateRockQuestion, functionality is implemented in SeedQuestions()

--- exercises/csharp/TriviaGame/TriviaGame/Game.cs
+++ exercises/csharp/TriviaGame/TriviaGame/Game.cs
@@ -8,27 +8,15 @@ namespace TriviaGame
     public class Game
     {
 
-        LinkedList<string> popQuestions = new LinkedList<string>();
-        LinkedList<string> scienceQuestions = new LinkedList<string>();
-        LinkedList<string> sportsQuestions = new LinkedList<string>();
-        LinkedList<string> rockQuestions = new LinkedList<string>();
-
         int currentPlayer = 0;
         bool isPlayerGettingOutOfPenaltyBox;
         List<Player> players = new List<Player>();
+        Questions questions = new Questions();
 
         public Game()
         {
-            for (int i = 0; i < 50; i++)
-            {
-                popQuestions.AddLast("Pop Question " + i);
-                scienceQuestions.AddLast(("Science Question " + i));
-                sportsQuestions.AddLast(("Sports Question " + i));
-                rockQuestions.AddLast(CreateRockQuestion(i));
-            }
         }
 
-        public string CreateRockQuestion(int index)
         public void AddPlayer(string playerName)
         {
             players.Add(new Player(playerName));
@@ -63,8 +51,7 @@ namespace TriviaGame
                     Console.WriteLine(players[currentPlayer]
                             + "'s new location is "
                             + playerPlaces[currentPlayer]);
-                    Console.WriteLine("The category is " + CurrentCategory());
-                    AskQuestion();
+                    questions.AskQuestion(currentPlayer.Place);
                 }
                 else
                 {
@@ -82,49 +69,15 @@ namespace TriviaGame
                 Console.WriteLine(players[currentPlayer]
                         + "'s new location is "
                         + playerPlaces[currentPlayer]);
-                Console.WriteLine("The category is " + CurrentCategory());
-                AskQuestion();
+                questions.AskQuestion(currentPlayer.Place);
             }
 
         }
 
-        private void AskQuestion()
         {
-            if (CurrentCategory() == "Pop")
-            {
-                Console.WriteLine(popQuestions.First());
-                popQuestions.RemoveFirst();
-            }
-            if (CurrentCategory() == "Science")
-            {
-                Console.WriteLine(scienceQuestions.First());
-                scienceQuestions.RemoveFirst();
-            }
-            if (CurrentCategory() == "Sports")
-            {
-                Console.WriteLine(sportsQuestions.First());
-                sportsQuestions.RemoveFirst();
-            }
-            if (CurrentCategory() == "Rock")
-            {
-                Console.WriteLine(rockQuestions.First());
-                rockQuestions.RemoveFirst();
-            }
-        }
 
 
-        private string CurrentCategory()
         {
-            if (playerPlaces[currentPlayer] == 0) return "Pop";
-            if (playerPlaces[currentPlayer] == 4) return "Pop";
-            if (playerPlaces[currentPlayer] == 8) return "Pop";
-            if (playerPlaces[currentPlayer] == 1) return "Science";
-            if (playerPlaces[currentPlayer] == 5) return "Science";
-            if (playerPlaces[currentPlayer] == 9) return "Science";
-            if (playerPlaces[currentPlayer] == 2) return "Sports";
-            if (playerPlaces[currentPlayer] == 6) return "Sports";
-            if (playerPlaces[currentPlayer] == 10) return "Sports";
-            return "Rock";
         }
 
         public bool CorrectAnswer()
---
+++ exercises/csharp/TriviaGame/TriviaGame/Questions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TriviaGame
+{
+    public class Questions
+    {
+        public LinkedList<string> popQuestions = new LinkedList<string>();
+        public LinkedList<string> scienceQuestions = new LinkedList<string>();
+        public LinkedList<string> sportsQuestions = new LinkedList<string>();
+        public LinkedList<string> rockQuestions = new LinkedList<string>();
+
+        public Questions()
+        {
+            SeedQuestions();
+        }
+
+        public void SeedQuestions()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                popQuestions.AddLast("Pop Question " + i);
+                scienceQuestions.AddLast("Science Question " + i);
+                sportsQuestions.AddLast("Sports Question " + i);
+                rockQuestions.AddLast("Rock Question " + i);
+            }
+        }
+        public string GetCurrentQuestionCategory(int playerPlace)
+        {
+            switch (playerPlace)
+            {
+                case 0:
+                case 4:
+                case 8:
+                    return "Pop";
+                case 1:
+                case 5:
+                case 9:
+                    return "Science";
+                case 2:
+                case 6:
+                case 10:
+                    return "Sports";
+                default:
+                    return "Rock";
+            }
+        }
+
+        public void AskQuestion(int playerPlace)
+        {
+            string currentCategory = GetCurrentQuestionCategory(playerPlace);
+
+            if (currentCategory == "Pop")
+            {
+                Console.WriteLine(popQuestions.First());
+                popQuestions.RemoveFirst();
+            }
+            if (currentCategory == "Science")
+            {
+                Console.WriteLine(scienceQuestions.First());
+                scienceQuestions.RemoveFirst();
+            }
+            if (currentCategory == "Sports")
+            {
+                Console.WriteLine(sportsQuestions.First());
+                sportsQuestions.RemoveFirst();
+            }
+            if (currentCategory == "Rock")
+            {
+                Console.WriteLine(rockQuestions.First());
+                rockQuestions.RemoveFirst();
+            }
+        }
+    }
+}
