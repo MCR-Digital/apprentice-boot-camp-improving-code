de1501a487db770cdf3d3fc2265008e4a432758e
Jacob Walsh <jacob.professional+github@protonmail.com>
2022-01-17 14:38:32 +0000

    Add comments to GameRunner.java and extract variables

--- exercises/java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ exercises/java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -11,18 +11,25 @@ public class GameRunner {
 
 	public static void main(String[] args) {
 		Game aGame = new Game();
-		
+
+		// Add 3 players with names
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
+
+		// Create new random number generator, with a seed of the first input argument
 		Random rand = new Random(Integer.parseInt(args[0]));
-	
+
+		// While no winner
 		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
-			if (rand.nextInt(9) == 7) {
+			// Call roll() with a random number between 1 inclusive and 6 exclusive
+			int fiveSidedDie = rand.nextInt(5) + 1;
+			aGame.roll(fiveSidedDie);
+
+			// Generate another random number between 0 inclusive and 9 exclusive
+			// Randomly choose if right or wrong
+			boolean isWrongAnswer = rand.nextInt(9) == 7;
+			if (isWrongAnswer) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
