e3b4cd8aeb8cd41a5acf7abc18cad5f3903698a3
Adam Rutherford-Shaw <adam.rutherfordshaw@autotrader.co.uk>
2022-01-18 12:24:10 +0000

    refactor: tidy up more code

--- exercises/nodejs/src/game.js
+++ exercises/nodejs/src/game.js
@@ -3,26 +3,57 @@
 import generator from "random-seed";
 
 var Game = function () {
-  var players = new Array();
-  var places = new Array(6);
-  var purses = new Array(6);
-  var inPenaltyBox = new Array(6);
+  const players = new Array();
+  const places = new Array(6);
+  const purses = new Array(6);
+  const inPenaltyBox = new Array(6);
+  
+  const popQuestions = new Array();
+  const scienceQuestions = new Array();
+  const sportsQuestions = new Array();
+  const rockQuestions = new Array();
+  
+  const numberOfCategories = 4;
+  const winningNumberofCoins = 6;
+  const boardSize = 12;
 
-  var popQuestions = new Array();
-  var scienceQuestions = new Array();
-  var sportsQuestions = new Array();
-  var rockQuestions = new Array();
-
-  var currentPlayer = 0;
-  var isGettingOutOfPenaltyBox = false;
+  let currentPlayer = 0;
+  let isGettingOutOfPenaltyBox = false;
 
   const checkPlayerHasWon = () => {
-    return !(purses[currentPlayer] === 6);
+    return !(purses[currentPlayer] === winningNumberofCoins);
+  };
+
+  const getCurrentPlayer = () => {
+    return players[currentPlayer];
+  };
+
+  const getCurrentLocation = () => {
+    return places[currentPlayer];
+  };
+
+  this.getNumberOfPlayers = function () {
+    return players.length;
+  };
+
+  this.getLastPlayer = function () {
+    return this.getNumberOfPlayers() - 1;
+  };
+
+  this.setLocation = function (player, position) {
+    places[player] = position;
+  };
+
+  this.setGoldCoins = function (player, coins) {
+    purses[player] = coins;
+  };
+
+  this.setPenaltyBox = function (player, inBox) {
+    inPenaltyBox[player] = inBox;
   };
 
   const currentCategory = () => {
-    const numberOfCategories = 4;
-    const categoryNumber = places[currentPlayer] % numberOfCategories;
+    const categoryNumber = getCurrentLocation() % numberOfCategories;
 
     switch (categoryNumber) {
       case 0:
@@ -36,36 +67,32 @@ var Game = function () {
     }
   };
 
-  const createRockQuestion = (category, index) => {
+  const createQuestion = (category, index) => {
     return `${category} Question ${index}`;
   };
 
   for (var i = 0; i < 50; i++) {
-    popQuestions.push(createRockQuestion("Pop", i));
-    scienceQuestions.push(createRockQuestion("Science", i));
-    sportsQuestions.push(createRockQuestion("Sports", i));
-    rockQuestions.push(createRockQuestion("Rock", i));
+    popQuestions.push(createQuestion("Pop", i));
+    scienceQuestions.push(createQuestion("Science", i));
+    sportsQuestions.push(createQuestion("Sports", i));
+    rockQuestions.push(createQuestion("Rock", i));
   }
 
-  const checkIsPlayable = (howManyPlayers) => {
+  this.checkIsPlayable = function (howManyPlayers) {
     const minimumPlayers = 2;
     return howManyPlayers >= minimumPlayers;
   };
 
   this.addPlayer = function (playerName) {
     players.push(playerName);
-    places[this.getNumberOfPlayers() - 1] = 0;
-    purses[this.getNumberOfPlayers() - 1] = 0;
-    inPenaltyBox[this.getNumberOfPlayers() - 1] = false;
+    this.setLocation(this.getLastPlayer(), 0);
+    this.setGoldCoins(this.getLastPlayer(), 0);
+    this.setPenaltyBox(this.getLastPlayer(), false);
 
     console.log(playerName + " was added");
-    console.log("They are player number " + players.length);
+    console.log("They are player number " + this.getNumberOfPlayers());
 
     return true;
-  };
-
-  this.getNumberOfPlayers = function () {
-    return players.length;
   };
 
   const askQuestion = () => {
@@ -86,9 +113,9 @@ var Game = function () {
   };
 
   const movePlayer = (roll) => {
-    places[currentPlayer] = places[currentPlayer] + roll;
-    if (places[currentPlayer] > 11) {
-      places[currentPlayer] = places[currentPlayer] - 12;
+    this.setLocation(currentPlayer, getCurrentLocation() + roll);
+    if (getCurrentLocation() > 11) {
+      this.setLocation(currentPlayer, getCurrentLocation() - boardSize);
     }
 
     console.log(
@@ -98,11 +125,16 @@ var Game = function () {
     askQuestion();
   };
 
+  this.getInPenaltyBox = function () {
+    return inPenaltyBox[currentPlayer];
+  };
+
   this.roll = function (roll) {
     console.log(players[currentPlayer] + " is the current player");
     console.log("They have rolled a " + roll);
+    const rollIsOddNumber = roll % 2 !== 0;
 
-    if (inPenaltyBox[currentPlayer]) {
+    if (this.getInPenaltyBox()) {
       if (roll % 2 !== 0) {
         isGettingOutOfPenaltyBox = true;
 
@@ -130,15 +162,18 @@ var Game = function () {
     return true;
   };
 
+  this.canAnswerQuestion = () =>
+    !this.getInPenaltyBox() || isGettingOutOfPenaltyBox;
+
   this.wasCorrectlyAnswered = function () {
     const addGoldCoin = () => {
       console.log("Answer was correct!!!!");
       purses[currentPlayer] += 1;
       console.log(
-        players[currentPlayer] +
-        " now has " +
-        purses[currentPlayer] +
-        " Gold Coins."
+        getCurrentPlayer() +
+          " now has " +
+          purses[currentPlayer] +
+          " Gold Coins."
       );
 
       var winner = checkPlayerHasWon();
@@ -150,14 +185,10 @@ var Game = function () {
       return winner;
     };
 
-    if (inPenaltyBox[currentPlayer]) {
-      if (isGettingOutOfPenaltyBox) {
-        return addGoldCoin();
-      } else {
-        return nextPlayer();
-      }
-    } else {
+    if (this.canAnswerQuestion()) {
       return addGoldCoin();
+    } else {
+      return nextPlayer();
     }
   };
 
@@ -171,9 +202,9 @@ var Game = function () {
 };
 
 const gameRunner = (i) => {
-  var hasWonGame = false;
-
-  var game = new Game();
+  const game = new Game();
+  
+  let hasWonGame = false;
 
   game.addPlayer("Chet");
   game.addPlayer("Pat");
