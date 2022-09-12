e8d0f5d96512708e431e0a2e3529863248edaad0
Dalen Chan <Dalen.Chan@brighthr.com>
2022-09-12 14:03:51 +0100

    Renamed Uncommincative names

--- exercises/csharp/TriviaGame/TriviaGame/GameRunner.cs
+++ exercises/csharp/TriviaGame/TriviaGame/GameRunner.cs
@@ -1,44 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TriviaGame
 {
    public class GameRunner
     {
 
-        private static bool notAWinner;
+        private static bool isNotAWinner;
 
         public static void Main(String[] args)
         {
-            Game aGame = new Game();
+            Game game = new Game();
 
-            aGame.Add("Chet");
-            aGame.Add("Pat");
-            aGame.Add("Sue");
+            game.AddPlayer("Chet");
+            game.AddPlayer("Pat");
+            game.AddPlayer("Sue");
 
-            Random rand = new Random(Int32.Parse(args[0]));
+            Random randomNumber = new Random(Int32.Parse(args[0]));
 
             do
             {
 
-                aGame.Roll(rand.Next(5) + 1);
+                game.Roll(randomNumber.Next(5) + 1);
 
-                if (rand.Next(9) == 7)
+                if (randomNumber.Next(9) == 7)
                 {
-                    notAWinner = aGame.WrongAnswer();
+                    isNotAWinner = game.IsWrongAnswer();
                 }
                 else
                 {
-                    notAWinner = aGame.WasCorrectlyAnswered();
+                    isNotAWinner = game.IsCorrectAnswer();
                 }
 
 
 
-            } while (notAWinner);
+            } while (isNotAWinner);
 
         }
     }
